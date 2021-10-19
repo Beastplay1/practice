743ecc9cd25ec387e033ecafcc452066ee008af5
Beastplay1 <91196171+Beastplay1@users.noreply.github.com>
2021-10-19 12:45:04 +0400

    Add files via upload

--- praktika/js/main.js
+++ praktika/js/main.js
@@ -1,47 +1,54 @@
-$(function(){
-	var lift1 = $(".lift1");
-	var lift2 = $(".lift2");
-	var lift3 = $(".lift3");
-	var but = $(".man-button");
-	var floor = [1,2,3,4,5,6,7,8,9];
+let divs = document.getElementsByTagName('div');
 
-	$('.lift').each(function(i,el){
-    //    var tBottom = Math.floor(Math.random()*500),
-      var tTop  = Math.floor(Math.random()*500);
-	  var sTop = Math.floor(Math.random()*500);
-	  var fTop = Math.floor(Math.random()*500);
-	  var mTop = Math.floor(Math.random()*500);
-	  var man = Math.floor(Math.random()*floor.length);
-	  var fMan = Math.floor(Math.random()*floor.length);
-	  var sMan = Math.floor(Math.random()*floor.length);
-	  var tMan = Math.floor(Math.random()*floor.length);
+let layn = window.innerHeight;
 
-      lift1.css({position:'relative', top: tTop});
-      lift2.css({position:'relative', top: sTop});
-      lift3.css({position:'relative', top: fTop});
+let n=0
+
+let maxtop=""
+
+for ( let i=0; i < divs.length; i++ ) {
+ 	
+    let thisDiv = divs[i];
+    
+    randomTop = myfunc2(0, layn);
+    thisDiv.style.top = randomTop +"px";
+if(n==0){
+  n=randomTop
+}
+
+if(n>=randomTop && thisDiv.id!="mard"){
+ maxtop=thisDiv
+ n=randomTop
+}
+
+}
+
+
+
+function myfunc2() {
+    
+  return Math.floor(Math.random() * 900);
+    
+}
+
+let m = document.getElementById("mard");
+
+let l = m.style.position=28;
+
+
+
+function myfunc(){
+    let pos = n;
+    let id = setInterval(frame);
+    function frame(){
+     if(pos<=l){
+      clearInterval(id);
+     }else{
+      pos--;
+      }
       
-
-
-
-	  	but.click(function(){ 
-
-      if (tTop <= mTop || tTop > sTop && tTop > fTop) {
-				lift1.animate({position:'relative', top: "-100px"});
+    maxtop.style.top = pos + 'px';
       }
-
-      else if (sTop <= mTop || sTop > tTop && sTop > fTop) {
-				lift2.animate({position:'relative', top: "-100px"});
-
-      }
-
-      else if (fTop <= mTop || fTop > sTop &&  fTop > tTop) {
-      	lift3.animate({position:'relative', top: "-100px"});
-
-      }
-
-	});
-
-
-});
-
-});
+    
+    
+  }
